edee94bca2281eb6c380253620fa63916c4e3692
Aryan-Hestabit <aryan.hestabit@gmail.com>
2026-01-12 11:51:39 +0530

    Edited eslint config files.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,15 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    rules: {
+      semi: ["error", "always"],
+      "no-unused-vars": "error",
+      "space-in-parens": ["error", "never"],
+    },
+  },
+];
+
